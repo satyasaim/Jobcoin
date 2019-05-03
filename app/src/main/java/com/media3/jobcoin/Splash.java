40f7979f210afa9ce7e911b12a3a0bf2667a900d
ArunKumar8482 <arunkumarnese@gmail.com>
2019-05-03 15:21:15 +0530

    added splash screen and modified login activity UI

---
+++ app/src/main/java/com/media3/jobcoin/Splash.java
@@ -0,0 +1,34 @@
+package com.media3.jobcoin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new  Thread(){
+            public void run(){
+
+
+                try {
+                    sleep(3000);
+
+                    Intent splashIntent = new Intent(Splash.this, Login.class);
+                    startActivity(splashIntent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+    }
+
+
+}
